431493e4addabd46ec205ec210ee235a1ffe8593
beamatos20 <osorio.beatriz@escola.pr.gov.br>
2026-06-16 15:43:23 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+// Mensagem de boas-vindas
+console.log("Bem-vindo ao site Agro forte, futuro sustentável!");
+
+// Função simples
+function mostrarMensagem() {
+    alert("Obrigado por visitar nosso site sobre sustentabilidade no agronegócio!");
+}
